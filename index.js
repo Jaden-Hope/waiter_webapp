5eb7955497ade9fa219863e614eaac16e07231e6
Jaden Hope <Jaden_Hope@Yahoo.com>
2021-11-02 14:45:18 +0200

    heroku db

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const { Pool } = require('pg');
 
 const app = express();
 
+const connectionString = 'postgres://jfdsoropaifltq:3ed2c2dad843d0a370f96846cefd39468dd68a56b20ff2ad87fbb093b515225d@ec2-44-199-158-170.compute-1.amazonaws.com:5432/d1vt5opksmcfkk';
+
+const pool = new Pool({
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
 app.use(session({
     secret: 'keyboard cat5 run all 0v3r',
     resave: false,
